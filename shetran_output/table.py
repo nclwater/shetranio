bb85c3070f318681b621b599991e515f70841c61
fmcclean <fergus.mcclean@gmail.com>
2017-09-30 09:08:46 +0100

    plot3d working, had to change axes syntax

--- shetran_output/table.py
+++ shetran_output/table.py
@@ -3,6 +3,8 @@ import numpy as np
 import h5py
 import os
 import datetime
+from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
 
 
 def plot(h5_file, hdf_group, out_dir, timeseries_locations, start_date):
@@ -176,6 +178,7 @@ def plot(h5_file, hdf_group, out_dir, timeseries_locations, start_date):
     timeseriesplot(datetimes, data, Npoints, row, col, elevation)
 
 def plot2d(h5_file, time_interval, time, hdf_group, out_dir):
+
     """Using HDF file, produces Time Series of phreatic surface depth at timeseriesLocations
 
         Args:
@@ -327,3 +330,171 @@ def plot2d(h5_file, time_interval, time, hdf_group, out_dir):
     # 2d plots. The numbers produced depend on the time interval
     print '2d plot'
     TwoDPlot(time, ntimes, nrows, ncols, minpsl, maxpsl, GridSize, time_interval, hdf_group, out_dir)
+
+def plot3d(h5_file, hdf_group, out_dir):
+    """Using HDF file, produces 3d plots of water table or phreatic surface depth.
+        The face colour corresponds to the phreatic depth.
+        By default it is produced at the final timestep (ntimes) with views every 10 degrees.
+
+            Args:
+                h5_file (str): Path to the input HDF5 file.
+                hdf_group (str): Name of HDF file output group.
+                out_dir (str): Folder to save the output PNG into.
+
+            Returns:
+                None
+
+        """
+
+    # assume grid size is the same everywhere (this is not necessarily true but is usual)
+    def getGridSize():
+
+        tgroup = '/CONSTANTS'
+        tsubgroup = 'grid_dxy'
+
+        group = fh5[tgroup]
+        # print group
+
+        for subgroup in group:  # iterate over subgroups
+            # print subgroup
+            if tsubgroup in subgroup:
+                # print 'found required subgroup: ' , subgroup
+
+                val = group[subgroup]
+                Gridsize = np.nanmax(val)
+                # print Gridsize
+
+        return Gridsize
+
+    def getElevations():
+
+        tgroup = '/CONSTANTS'
+        tsubgroup = 'surf_elv'
+
+        group = fh5[tgroup]
+        # print group
+
+        for subgroup in group:  # iterate over subgroups
+            # print subgroup
+            if tsubgroup in subgroup:
+                # print 'found required subgroup: ' , subgroup
+
+                val = group[subgroup]
+                dims = val.shape
+                # Shetran adds extra column and row around grid. Only need dem in normal grid
+                nrows = dims[0] - 1
+                ncols = dims[1] - 1
+                dem = val[1:nrows, 1:ncols, 0]
+
+        return np.array(dem), nrows, ncols
+
+    def getpsl(t, nrows, ncols, HDFgroup):
+
+        tgroup = '/VARIABLES'
+        tsubgroup = HDFgroup
+
+        group = fh5[tgroup]
+
+        for subgroup in group:  # iterate over subgroups
+            if tsubgroup in subgroup:
+                # print 'found required subgroup: ' , subgroup
+                val = group[subgroup + '/' + 'value']
+
+                # inputs[nrows:ncols:time] ie [j,i,:]
+                data = val[1:nrows, 1:ncols, t]
+
+        return data
+
+    def getpsltimes(HDFgroup):
+
+        tgroup = '/VARIABLES'
+        tsubgroup = HDFgroup
+
+        group = fh5[tgroup]
+
+        for subgroup in group:  # iterate over subgroups
+            if tsubgroup in subgroup:
+                # print 'found required subgroup: ' , subgroup
+                times = group[subgroup + '/' + 'time']
+                psltimes = times[:]
+
+        return psltimes
+
+    def ThreeDPlot(ntimes, nrows, ncols, GridSize, mindem, maxdem, dem, HDFgroup, outfilefolder):
+
+        X = np.arange(0, (ncols - 1) * GridSize, GridSize)
+        # print X.shape
+        # X = np.arange(1,ncols)
+        # print X.shape
+        Y = np.arange(0, (nrows - 1) * GridSize, GridSize)
+        X, Y = np.meshgrid(X, Y)
+
+        # repeated to produce a plot for each direction (azi)
+        print '3d plot'
+        # starting direction
+        azi = 0
+        while azi < 360:
+            fig = plt.figure(figsize=[12.0, 5.0])
+
+            # ax = plt.subplot(1, 1, 1, projection='3d')
+            ax = Axes3D(fig)
+            plt.title('Water Table Depth (m below ground)')
+            h5datapsl = getpsl(ntimes - 1, nrows, ncols, HDFgroup)
+            r1 = h5datapsl / h5datapsl.max()
+
+            # plot phreatic levels (m above ground)
+            # r2=dem-h5datapsl
+            # r3=r2/np.nanmax(r2)
+            # surf = ax.plot_surface(Y, X, dem, rstride=1, cstride=1, facecolors=cm.Blues_r(r3), shade=False)
+
+            surf = ax.plot_surface(Y, X, dem, rstride=1, cstride=1, facecolors=cm.Blues_r(r1), shade=False)
+            ax.view_init(elev=20., azim=azi)
+            ax.set_zlim(mindem, maxdem)
+            ax.set_xlabel('Distance(m)')
+            ax.set_ylabel('Distance(m)')
+            ax.set_zlabel('Elevation(m)')
+
+            # ax.yaxis.set_major_locator(LinearLocator(4))
+            # ax.xaxis.set_major_locator(LinearLocator(4))
+
+            # sets the scale bar. This is a bit of a fiddle
+            h5datapsl[h5datapsl == -1.0] = np.nan
+            min3dpsl = np.nanmin(h5datapsl)
+            max3dpsl = np.nanmax(h5datapsl)
+            scale = np.zeros(shape=(nrows - 1, ncols - 1))
+            scale[scale == 0.0] = min3dpsl
+            scale[0, 0] = max3dpsl
+            # m = cm.ScalarMappable(cmap=cm.YlGnBu)
+            m = cm.ScalarMappable(cmap=cm.Blues_r)
+            m.set_array(scale)
+            plt.colorbar(m)
+
+            plt.savefig(outfilefolder + '/' + 'WaterTable-3d-view' + str(azi) + '.png')
+            plt.close()
+            azi += 10
+        return
+
+    # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+    # make folder for graphs and outputs
+    if not os.path.exists(out_dir):
+        os.mkdir(out_dir)
+
+    fh5 = h5py.File(h5_file, 'r')
+    dem, nrows, ncols = getElevations()
+
+    # sets values outside mask to nan
+    dem[dem == -1] = np.nan
+    mindem = np.nanmin(dem)
+    maxdem = np.nanmax(dem)
+
+    GridSize = getGridSize()
+
+    # get times of output. ntimes is the final time
+    psltimes = getpsltimes(hdf_group)
+    dimstime = psltimes.shape
+    ntimes = dimstime[0]
+
+    # 3D surface plots - by default produced at final time
+    # figures produced by default at ntimes (this can be changed)
+    ThreeDPlot(ntimes, nrows, ncols, GridSize, mindem, maxdem, dem, hdf_group, out_dir)
