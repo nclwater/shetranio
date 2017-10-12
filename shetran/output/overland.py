6584413eed0c2349dbdc4cb6275775b626fb3d5d
fmcclean <fergus.mcclean@gmail.com>
2017-10-12 19:42:29 +0100

    removed nested functions and using hdf module

--- shetran/output/overland.py
+++ shetran/output/overland.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import os
-import h5py
+from .. import hdf
 import datetime
 
 def plot(h5_file, hdf_group, timeseries_locations, start_date, out_dir=None):
@@ -18,188 +18,77 @@ def plot(h5_file, hdf_group, timeseries_locations, start_date, out_dir=None):
             None
 
     """
+    h5 = hdf.Hdf(h5_file)
 
-    def getColRowLocation():
-        tgroup = '/CONSTANTS'
-        tsubgroup = 'number'
+    # ovr_flow reference is [channel number:face:time]
 
-        group = fh5[tgroup]
-        # print group
+    with open(timeseries_locations, 'r') as f:
+        points = [int(point) for point in f.readlines()[1:]]
 
-        for subgroup in group:  # iterate over subgroups
-            # print subgroup
-            if tsubgroup in subgroup:
-                # print 'found required subgroup: ' , subgroup
-
-                val = group[subgroup]
-                ColRowLocation1 = val[:, :, 5]
-                ColRowLocation2 = val[:, :, 6]
-                ColRowLocation3 = val[:, :, 7]
-                ColRowLocation4 = val[:, :, 8]
-
-        return np.array(ColRowLocation1), np.array(ColRowLocation2), np.array(ColRowLocation3), np.array(ColRowLocation4)
-
-
-    def getElevations():
-        tgroup = '/CONSTANTS'
-        tsubgroup = 'surf_elv'
-
-        group = fh5[tgroup]
-        # print group
-
-        for subgroup in group:  # iterate over subgroups
-            # print subgroup
-            if tsubgroup in subgroup:
-                # print 'found required subgroup: ' , subgroup
-
-                val = group[subgroup]
-                Elevation1 = val[:, :, 5]
-                Elevation2 = val[:, :, 6]
-
-        return np.array(Elevation1), np.array(Elevation2)
-
-
-    def getTimeSeriesFromPoint(i, HDFgroup):
-        tgroup = '/VARIABLES'
-        tsubgroup = HDFgroup
-
-        group = fh5[tgroup]
-
-        for subgroup in group:  # iterate over subgroups
-            if tsubgroup in subgroup:
-                # print 'found required subgroup: ' , subgroup
-                val = group[subgroup + '/' + 'value']
-
-                # inputs[channel number:face:time]
-                # use i-1 as i starets at 0
-                data = val[i - 1, :, :]
-
-        return data
-
-
-    def getOvertimes(HDFgroup):
-        tgroup = '/VARIABLES'
-        tsubgroup = HDFgroup
-
-        group = fh5[tgroup]
-
-        for subgroup in group:  # iterate over subgroups
-            if tsubgroup in subgroup:
-                # print 'found required subgroup: ' , subgroup
-                times = group[subgroup + '/' + 'time']
-                Overtimes = times[:]
-
-        return Overtimes
-
-
-    def timeseriesplot(psltimes, data, Npoints, OverlandLoc, ElevationLink):
-        plotlabel = np.empty(Npoints, dtype=object)
-        fig = plt.figure(figsize=[12.0, 5.0],
-                         dpi=300)
-        plt.subplots_adjust(bottom=0.2, right=0.75)
-        ax = plt.subplot(1, 1, 1)
-        i = 0
-        while i < Npoints:
-            if ElevationLink[i] != -999:
-                plotlabel[i] = 'River Link= %4s' % str(int(OverlandLoc[i])) + ' Elev= %7.2f m' % ElevationLink[i]
-                # plot m below ground
-                ax.plot(psltimes, data[i, :], label=plotlabel[i])
-                # plot absolute elevation
-                # ax.plot(psltimes,elevation[i]-inputs[i,:],label=plotlabel[i])
-            i += 1
-        # plot m below ground
-        ax.set_ylabel('Discharge (m$^3$/s)')
-        plt.xticks(rotation=70)
-        ax.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0., prop={'size': 8})
-        if out_dir:
-            plt.savefig(os.path.join(out_dir,'Discharge-timeseries.png'))
-        plt.show()
-
-
-    def getTimeSeriesNpoints(locations):
-        # skip over the headers
-        locations.readline()
-        i = 0
-        for line in locations:
-            line.rstrip().split(",")
-            i += 1
-        return i
-
-
-    def getTimeSeriesLocation(locations, Npoints):
-        # skip over the headers
-        locations.readline()
-        i = 0
-        x = np.zeros(shape=(Npoints))
-        for line in locations:
-            x[i] = line
-            i += 1
-        return x
-
-
-    # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-
-    # number of points (Npoints) in tuime series file
-    locations = open(timeseries_locations, "r")
-    Npoints = getTimeSeriesNpoints(locations)
-
-    # go back to start of file and get locations
-    locations.seek(0)
-    OverlandLoc = getTimeSeriesLocation(locations, Npoints)
-
-    fh5 = h5py.File(h5_file, 'r')
+    number_of_points = len(points)
 
     # find location and elevation of each channel link
     # river links in Shetran flow along the edge of the grid squares. The number starts at the bottom left then considers each row in turn going upwards.
     # There are north-south and east-west channels
-    ColRowLocation1, ColRowLocation2, ColRowLocation3, ColRowLocation4 = getColRowLocation()
-    Elevation1, Elevation2 = getElevations()
-    ElevationLink = np.zeros(shape=(Npoints))
-    i = 0
-    while i < Npoints:
-        p1 = np.where(ColRowLocation1 == int(OverlandLoc[i]))
-        p2 = np.where(ColRowLocation2 == int(OverlandLoc[i]))
-        p3 = np.where(ColRowLocation3 == int(OverlandLoc[i]))
-        p4 = np.where(ColRowLocation4 == int(OverlandLoc[i]))
+    ColRowLocation1, ColRowLocation2, ColRowLocation3, ColRowLocation4 = h5.number[:,:,5], h5.number[:,:,6], h5.number[:,:,7], h5.number[:,:,8]
+    Elevation1, Elevation2 = h5.surface_elevation[:,:,5], h5.surface_elevation[:,:,6]
+    elevation_link = np.zeros(shape=number_of_points)
+    for i in range(number_of_points):
+        p1 = np.where(ColRowLocation1 == int(points[i]))
+        p2 = np.where(ColRowLocation2 == int(points[i]))
+        p3 = np.where(ColRowLocation3 == int(points[i]))
+        p4 = np.where(ColRowLocation4 == int(points[i]))
         if p1[0] > 0:
-            ElevationLink[i] = Elevation1[p1]
+            elevation_link[i] = Elevation1[p1]
             # print str(int(OverlandLoc[i])) + ' is a E-W channel on column ' + str(p1[1])[1:-1] + ' between rows ' + str(
             #     p3[0])[1:-1] + ' and ' + str(p1[0])[1:-1] + ' with elevation = ' + str(Elevation1[p1])[1:-1]
         elif p2[0] > 0:
-            ElevationLink[i] = Elevation2[p2]
+            elevation_link[i] = Elevation2[p2]
             # print str(int(OverlandLoc[i])) + ' is a N-S channel on row ' + str(p2[0])[1:-1] + ' between columns ' + str(
             #     p2[1])[1:-1] + ' and  ' + str(p4[1])[1:-1] + ' with elevation = ' + str(Elevation2[p2])[1:-1]
         else:
-            ElevationLink[i] = -999
+            elevation_link[i] = -999
             # print str(int(OverlandLoc[i])) + ' is not a Shetran link number'
         i += 1
 
-    # get times of output. ntimes is the final time
-    Overtimes = getOvertimes(hdf_group)
-    dimstime = Overtimes.shape
-    ntimes = dimstime[0]
+    number_of_time_steps = len(h5.overland_flow_time)
 
     # setup a datetime array. there must be a better way than this
-    datetimes = np.array([start_date + datetime.timedelta(hours=i) for i in xrange(ntimes)])
-    i = 0
-    while i < ntimes:
-        datetimes[i] = start_date + datetime.timedelta(hours=int(Overtimes[i]))
-        i += 1
+    times = np.array([start_date + datetime.timedelta(hours=int(h5.overland_flow_time[:][i]))
+                      for i in range(number_of_time_steps)])
 
     # get the time series inputs
     # discharge is specifed at 4 faces. We want the maximum absolute discharge
-    data = np.zeros(shape=(Npoints, 4, ntimes))
-    data2 = np.zeros(shape=(Npoints, ntimes))
-    i = 0
-    while i < Npoints:
-        if ElevationLink[i] != -999:
-            data[i, :, :] = getTimeSeriesFromPoint(OverlandLoc[i], hdf_group)
-        i += 1
-    for i in range(0, Npoints):
-        for j in range(0, ntimes):
-            data2[i, j] = np.amax(abs(data[i, :, j]))
+    discharge_at_all_faces = np.zeros(shape=(number_of_points, 4, number_of_time_steps))
+    maximum_absolute_discharge = np.zeros(shape=(number_of_points, number_of_time_steps))
 
-    # time series plots.
-    # print 'time series plot'
-    timeseriesplot(datetimes, data2, Npoints, OverlandLoc, ElevationLink)
+    for i in range(number_of_points):
+        if elevation_link[i] != -999:
+            discharge_at_all_faces[i, :, :] = h5.overland_flow_value[points[i] - 1, :, :]
+        i += 1
+    for i in range(number_of_points):
+        for j in range(0, number_of_time_steps):
+            maximum_absolute_discharge[i, j] = np.amax(abs(discharge_at_all_faces[i, :, j]))
+
+    labels = np.empty(number_of_points, dtype=object)
+    plt.figure(figsize=[12.0, 5.0],
+               dpi=300)
+    plt.subplots_adjust(bottom=0.2, right=0.75)
+    ax = plt.subplot(1, 1, 1)
+
+    for idx in range(number_of_points):
+        if elevation_link[idx] != -999:
+            labels[idx] = 'River Link= %4s' % str(int(points[idx])) + ' Elev= %7.2f m' % elevation_link[idx]
+            # plot m below ground
+            ax.plot(times, maximum_absolute_discharge[idx, :], label=labels[idx])
+            # plot absolute elevation
+            # ax.plot(psltimes,elevation[i]-inputs[i,:],label=plotlabel[i])
+    # plot m below ground
+    ax.set_ylabel('Discharge (m$^3$/s)')
+    plt.xticks(rotation=70)
+    ax.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0., prop={'size': 8})
+    if out_dir:
+        if not os.path.exists(out_dir):
+            os.mkdir(out_dir)
+        plt.savefig(os.path.join(out_dir, 'Discharge-timeseries.png'))
+    plt.show()
