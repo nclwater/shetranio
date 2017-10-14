766004d460794dd01d90179694d3971190f88654
fmcclean <fergus.mcclean@gmail.com>
2017-10-14 21:05:34 +0100

    added get_element_location method

--- shetran/hdf.py
+++ shetran/hdf.py
@@ -52,3 +52,18 @@ class Hdf:
         y_index = int(np.where(y_coordinates==y_location)[0])
         return self.number[y_index,x_index,0]
 
+
+    def get_element_location(self, dem_file, element_number):
+        d = dem.Dem(dem_file)
+
+        index = np.where(self.number[:, :, 0] == element_number)
+
+
+        x_coordinates = np.array([d.x_lower_left+i*d.cell_size for i in range(d.number_of_columns)])
+        y_coordinates = np.array([d.y_lower_left+i*d.cell_size for i in range(d.number_of_rows)])
+
+        x_location = x_coordinates[int(index[1])]
+        y_location = y_coordinates[int(index[0])]
+
+        return x_location,y_location
+
