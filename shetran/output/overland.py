1435c65b40df341e1eb755d357fee3a87f5f7c45
fmcclean <fergus.mcclean@gmail.com>
2017-10-14 19:39:04 +0100

    renamed ColRowLocations to element_numbers

--- shetran/output/overland.py
+++ shetran/output/overland.py
@@ -25,28 +25,36 @@ def plot(h5_file, hdf_group, timeseries_locations, start_date, out_dir=None):
     with open(timeseries_locations, 'r') as f:
         points = [int(point) for point in f.readlines()[1:]]
 
+    # This is where conversion from coordinates to element numbers needs to happen
+    # Need the lower left coordinates of the grid, then get the nearest cell to the given coordinates
+
     number_of_points = len(points)
 
     # find location and elevation of each channel link
     # river links in Shetran flow along the edge of the grid squares. The number starts at the bottom left then considers each row in turn going upwards.
     # There are north-south and east-west channels
-    ColRowLocation1, ColRowLocation2 = h5.number[:,:,5], h5.number[:,:,6]
+    # number[x_index,y_index,channel_direction]
+    # 5 seems to be north-south channels and 6 east west
+    # The same applies to surface elevation
+    # This section seems to just check if the cells are channels or not
+    north_south_element_numbers, east_west_element_numbers = h5.number[:,:,5], h5.number[:,:,6]
 
     Elevation1, Elevation2 = h5.surface_elevation[:,:,5], h5.surface_elevation[:,:,6]
-    elevation_link = []
+    elevation_links = []
     for point in points:
-        p1 = np.where(ColRowLocation1 == point)
-        p2 = np.where(ColRowLocation2 == point)
-        if p1[0] > 0:
-            elevation_link.append(Elevation1[p1])
+
+        if point in north_south_element_numbers:
+            north_south_element_index = np.where(north_south_element_numbers == point)
+            elevation_links.append(Elevation1[north_south_element_index])
             # print str(int(OverlandLoc[i])) + ' is a E-W channel on column ' + str(p1[1])[1:-1] + ' between rows ' + str(
             #     p3[0])[1:-1] + ' and ' + str(p1[0])[1:-1] + ' with elevation = ' + str(Elevation1[p1])[1:-1]
-        elif p2[0] > 0:
-            elevation_link.append(Elevation2[p2])
+        elif point in east_west_element_numbers:
+            east_west_element_index = np.where(east_west_element_numbers == point)
+            elevation_links.append(Elevation2[east_west_element_index])
             # print str(int(OverlandLoc[i])) + ' is a N-S channel on row ' + str(p2[0])[1:-1] + ' between columns ' + str(
             #     p2[1])[1:-1] + ' and  ' + str(p4[1])[1:-1] + ' with elevation = ' + str(Elevation2[p2])[1:-1]
         else:
-            elevation_link.append(-999)
+            elevation_links.append(-999)
             # print str(int(OverlandLoc[i])) + ' is not a Shetran link number'
 
     number_of_time_steps = len(h5.overland_flow_time)
@@ -61,24 +69,24 @@ def plot(h5_file, hdf_group, timeseries_locations, start_date, out_dir=None):
     maximum_absolute_discharge = np.zeros(shape=(number_of_points, number_of_time_steps))
 
     for i in range(number_of_points):
-        if elevation_link[i] != -999:
+        if elevation_links[i] != -999:
+            # Why is 1 being subtracted from the element number?
             discharge_at_all_faces[i, :, :] = h5.overland_flow_value[points[i] - 1, :, :]
         i += 1
     for i in range(number_of_points):
         for j in range(0, number_of_time_steps):
             maximum_absolute_discharge[i, j] = np.amax(abs(discharge_at_all_faces[i, :, j]))
 
-    labels = np.empty(number_of_points, dtype=object)
     plt.figure(figsize=[12.0, 5.0],
                dpi=300)
     plt.subplots_adjust(bottom=0.2, right=0.75)
     ax = plt.subplot(1, 1, 1)
 
     for idx in range(number_of_points):
-        if elevation_link[idx] != -999:
-            labels[idx] = 'River Link= %4s' % str(int(points[idx])) + ' Elev= %7.2f m' % elevation_link[idx]
+        if elevation_links[idx] != -999:
             # plot m below ground
-            ax.plot(times, maximum_absolute_discharge[idx, :], label=labels[idx])
+            ax.plot(times, maximum_absolute_discharge[idx, :],
+                    label='River Link= %4s' % str(int(points[idx])) + ' Elev= %7.2f m' % elevation_links[idx])
             # plot absolute elevation
             # ax.plot(psltimes,elevation[i]-inputs[i,:],label=plotlabel[i])
     # plot m below ground
