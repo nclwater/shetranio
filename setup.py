c0b999d5cd3482d129cc1bdd5b9f896ae5ff5b13
fmcclean <MtQ-wmg-geY-eo6>
2017-12-06 13:41:09 +0000

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='shetran',
+    version='0.1',
+    packages=['shetran', 'shetran.plot'],
+    url='https://github.com/fmcclean/shetran',
+    license='MIT',
+    author='Fergus McClean',
+    author_email='f.mcclean2@ncl.ac.uk',
+    description='A python package to complement the SHETRAN hydrological model.'
+)
