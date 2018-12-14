044867d81449adf5affc5dd3b55c8779aedac01a
fmcclean <f.mcclean2@ncl.ac.uk>
2018-12-14 10:55:09 +0000

    fixed coords by flipping y, adding boundary cells & getting centroids

--- shetran/dem.py
+++ shetran/dem.py
@@ -10,11 +10,14 @@ class Dem:
         self.x_lower_left = float(lines[2].split()[1])
         self.y_lower_left = float(lines[3].split()[1])
         self.cell_size = int(lines[4].split()[1])
-        self.x_coordinates = np.array([self.x_lower_left + i * self.cell_size for i in range(self.number_of_columns)])
-        self.y_coordinates = np.array([self.y_lower_left + i * self.cell_size for i in range(self.number_of_rows)])
+        self.x_coordinates = np.array([self.x_lower_left - self.cell_size + self.cell_size/2 + i * self.cell_size
+                                       for i in range(self.number_of_columns + 2)])
+        self.y_coordinates = np.flip(np.array([self.y_lower_left - self.cell_size + self.cell_size/2 + i * self.cell_size
+                                               for i in range(self.number_of_rows + 2)]), axis=0)
 
     def get_index(self, x, y):
-        x_location = self.x_coordinates[np.abs(self.x_coordinates - x) == np.min(np.abs(self.x_coordinates - x))]
-        y_location = self.y_coordinates[np.abs(self.y_coordinates - y) == np.min(np.abs(self.y_coordinates - y))]
+
+        x_location = list(self.x_coordinates[np.abs(self.x_coordinates - x) == np.min(np.abs(self.x_coordinates - x))])[0]
+        y_location = list(self.y_coordinates[np.abs(self.y_coordinates - y) == np.min(np.abs(self.y_coordinates - y))])[0]
 
         return int(np.where(self.x_coordinates == x_location)[0]), int(np.where(self.y_coordinates == y_location)[0])
