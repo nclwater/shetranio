c38daf99922cc24aa371921c24d05b6528e5e7d1
fmcclean <fergus.mcclean@gmail.com>
2019-04-14 15:33:20 +0100

    connected map and plot with clickedElement signal

--- ui/ui.py
+++ ui/ui.py
@@ -1,10 +1,11 @@
 import sys
-from shetran.hdf import Hdf
+from shetran.hdf import Hdf, Geometries
 from shetran.dem import Dem
 import argparse
 from pyqtlet import L, MapWidget
 
-from PyQt5.QtWidgets import QApplication, QMainWindow, QSizePolicy, QPushButton, QFileDialog, QVBoxLayout, QWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow, QSizePolicy, QLineEdit, QPushButton, QFileDialog, QVBoxLayout, QWidget
+from PyQt5.QtCore import pyqtSignal, pyqtSlot, QJsonValue
 
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
@@ -21,63 +22,51 @@ class App(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        if not hasattr(args, 'h5'):
-            options = QFileDialog.Options()
+        for attribute, fileClass in [['h5', Hdf], ['dem', Dem]]:
 
-            fileName, _ = QFileDialog.getOpenFileName(self, "QFileDialog.getOpenFileName()", "",
-                                                      "All Files (*);;Python Files (*.py)", options=options)
-            if fileName:
-                print(fileName)
-                self.h5 = Hdf(fileName)
+            if not hasattr(args, attribute):
+                options = QFileDialog.Options()
 
-        else:
-            self.h5 = Hdf(args.h5)
+                fileName, _ = QFileDialog.getOpenFileName(self, "QFileDialog.getOpenFileName()", "",
+                                                          "All Files (*);;Python Files (*.py)", options=options)
+                if fileName:
+                    self.__setattr__(attribute, fileClass(fileName))
 
-        if not hasattr(args, 'dem'):
-            options = QFileDialog.Options()
+            else:
+                self.__setattr__(attribute, fileClass(args.__getattribute__(attribute)))
 
-            fileName, _ = QFileDialog.getOpenFileName(self, "QFileDialog.getOpenFileName()", "",
-                                                      "All Files (*);;Python Files (*.py)", options=options)
-            if fileName:
-                print(fileName)
-                self.dem = Dem(fileName)
-
-        else:
-            self.dem = Dem(args.dem)
-
-        self.left = 10
-        self.top = 10
+        self.left = 0
+        self.top = 0
         self.title = 'SHETran Results Viewer'
-        self.width = 1200
-        self.height = 700
-        self.initUI()
+        self.width = 1000
+        self.height = 500
 
-    def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
-        m = PlotCanvas(self, width=5, height=4)
-        m.move(0, 0)
+        self.plotCanvas = PlotCanvas(self, width=5, height=4)
 
-        map = MapCanvas(self)
+        self.mapCanvas = MapCanvas(self)
 
-        button = QPushButton('Next Element', self)
-        button.setToolTip('show data from the next element')
-        button.move(500, 0)
-        button.resize(140, 100)
-
-        def update_data():
-
-            m.element += 1
-
-            new_data = self.h5.overland_flow.values[m.element, 0, :]
-
-            m.line[0].set_data(range(len(new_data)), new_data)
-
-            m.draw()
-
-        button.clicked.connect(update_data)
+        self.mapCanvas.clickedElement.connect(self.update_data)
 
         self.show()
+
+    def update_data(self, element):
+
+        idx = self.h5.unique_numbers.tolist().index(element)
+
+        if idx > self.h5.overland_flow.values.shape[0]:
+            return
+
+        self.plotCanvas.element = element
+
+        new_data = self.h5.overland_flow.values[idx, 0, :]
+
+        self.plotCanvas.line[0].set_data(range(len(new_data)), new_data)
+        self.plotCanvas.axes.relim()
+        self.plotCanvas.axes.autoscale_view()
+        self.plotCanvas.axes.set_title('{} Overland Flow'.format(self.plotCanvas.element))
+        self.plotCanvas.draw()
 
 
 class PlotCanvas(FigureCanvas):
@@ -93,34 +82,64 @@ class PlotCanvas(FigureCanvas):
         FigureCanvas.setSizePolicy(self,
                                    QSizePolicy.Expanding,
                                    QSizePolicy.Expanding)
-        FigureCanvas.updateGeometry(self)
-        self.plot()
+        self.setGeometry(10,10,480,480)
+        fig.tight_layout()
 
-    def plot(self):
         data = self.parent().h5.overland_flow.values[self.element, 0, :]
         ax = self.figure.add_subplot(111)
         self.line = ax.plot(data, 'r-')
-        ax.set_title('Overland Flow')
+        ax.set_title('{} Overland Flow'.format(self.element))
         self.draw()
 
+
 class MapCanvas(QWidget):
+    clickedElement = pyqtSignal(int)
+
     def __init__(self, parent=None):
         self.mapWidget = MapWidget()
         QWidget.__init__(self, self.mapWidget)
         self.setParent(parent)
-        self.setGeometry(500,100,500,500)
+        self.setGeometry(500,0,500,500)
 
         self.layout = QVBoxLayout(self)
         self.layout.addWidget(self.mapWidget)
         self.setLayout(self.layout)
 
-        # Working with the maps with pyqtlet
         self.map = L.map(self.mapWidget)
         self.map.setView([12.97, 77.59], 10)
+
         L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(self.map)
-        self.marker = L.marker([12.934056, 77.610029])
-        self.marker.bindPopup('Hello World')
-        self.map.addLayer(self.marker)
+
+        geoms = Geometries(self.parent().h5, self.parent().dem)
+
+        class Element(L.polygon):
+
+            @pyqtSlot(QJsonValue)
+            def _signal(self, event):
+                self.mapWidget.parent().clickedElement.emit(self.property('name'))
+
+            def __init__(self, latLngs, element_number):
+                super().__init__(latLngs)
+                self.setProperty('name', element_number)
+                self._connectEventToSignal('click', '_signal')
+
+        group = L.featureGroup()
+        group.addTo(self.map)
+
+        for geom, number in zip(geoms, self.parent().h5.unique_numbers):
+            coords = [list(reversed(coord)) for coord in geom['coordinates'][0]]
+            group.addLayer(Element(coords, number))
+
+        def pan_to(bounds):
+            ne = bounds['_northEast']
+
+            sw = bounds['_southWest']
+            latlng = [sw['lat']+(ne['lat']-sw['lat'])/2, sw['lng']+(ne['lng']-sw['lng'])/2]
+            self.map.panTo(latlng)
+
+        group.getJsResponse('{}.getBounds()'.format(group.jsName), pan_to)
+
+
         self.show()
 
 
