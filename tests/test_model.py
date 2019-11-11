31731a67b75168148c1e4519b0c44c100d2dc4d7
Fergus McClean <fergus.mcclean@ncl.ac.uk>
2019-11-11 18:33:43 +0000

    fixed name of TestModel class

--- tests/test_model.py
+++ tests/test_model.py
@@ -10,7 +10,7 @@ def path(s):
     return os.path.join(sample_data, s)
 
 
-class TestHdf(unittest.TestCase):
+class TestModel(unittest.TestCase):
 
     def test_model(self):
         self.assertIsInstance(Model(path('Wansbeck_at_Mitford_Library_File.xml')), Model)
