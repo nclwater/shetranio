c44e8633697565e67c626c78baeef8b0368438f3
Fergus McClean <fergus.mcclean@ncl.ac.uk>
2019-11-11 17:35:44 +0000

    removed plot directory and updated setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
 from setuptools import setup
 
 setup(
-    name='shetran',
-    version='0.1',
-    packages=['shetran', 'shetran.plot'],
-    url='https://github.com/fmcclean/shetran',
+    name='shetranio',
+    version='0.1.0',
+    packages=['shetran', 'shetran.setup'],
+    url='https://github.com/nclwater/shetranio',
     license='MIT',
     author='Fergus McClean',
-    author_email='f.mcclean2@ncl.ac.uk',
-    description='A python package to complement the SHETRAN hydrological model.', install_requires=['numpy',
-                                                                                                    'matplotlib',
-                                                                                                    'pandas', 'PyQt5',
-                                                                                                    'pyqtlet']
+    author_email='fergus.mcclean@ncl.ac.uk',
+    description='A python package to complement the SHETran hydrological model.',
+    install_requires=[
+        'numpy',
+        'h5py',
+        'matplotlib',
+        'pandas',
+        'beautifulsoup4'
+    ]
 )
